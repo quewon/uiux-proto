fc62bfe1d193bc52f34c84aa748c3478d0cc4c6f
quewon <36914642+quewon@users.noreply.github.com>
2022-12-15 01:15:41 +0900

    better screenfitting for mobile

--- screenfitter.js
+++ screenfitter.js
@@ -1,26 +1,47 @@
-window.onresize = function() {
-  var width = document.documentElement.clientWidth;
-  var height = document.documentElement.clientHeight;
-  
-  // const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  // const height = window.innerHeight || document.documentElement.clientHeight|| document.body.clientHeight;
-  
-  console.log(width, height);
-  
-  const everything = document.getElementsByClassName("everything")[0];
-  const style = window.getComputedStyle(everything);
-  const sw = parseInt(style.getPropertyValue("width"));
-  const sh = parseInt(style.getPropertyValue("height"));
-  
-  var scaleX = width / sw;
-  var scaleY = height / sh;
-  
-  var scale = scaleX < scaleY ? scaleX : scaleY;
-  
-  console.log(scale)
-  
-  everything.style.transform = "translate(-50%,-50%) scale("+scale+")";
-  everything.style.display = "block";
+if ('ontouchstart' in document.documentElement) {
+  window.onresize = function() {
+    var width = document.documentElement.clientWidth;
+    var height = document.documentElement.clientHeight;
+    
+    const everything = document.getElementsByClassName("everything")[0];
+    const style = window.getComputedStyle(everything);
+    const sw = parseInt(style.getPropertyValue("width"));
+    const sh = parseInt(style.getPropertyValue("height"));
+    
+    var scale = width / sw;
+    
+    // width of everything should equal width
+    
+    everything.style.setProperty("--height", height / scale + "px");
+    
+    // x = sw / width
+    
+    everything.style.transform = "translate(-50%,-50%) scale("+scale+")";
+    everything.style.display = "block";
+  }
+} else {
+  window.onresize = function() {
+    var width = document.documentElement.clientWidth;
+    var height = document.documentElement.clientHeight;
+    
+    // const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    // const height = window.innerHeight || document.documentElement.clientHeight|| document.body.clientHeight;
+    
+    console.log(width, height);
+    
+    const everything = document.getElementsByClassName("everything")[0];
+    const style = window.getComputedStyle(everything);
+    const sw = parseInt(style.getPropertyValue("width"));
+    const sh = parseInt(style.getPropertyValue("height"));
+    
+    var scaleX = width / sw;
+    var scaleY = height / sh;
+    
+    var scale = scaleX < scaleY ? scaleX : scaleY;
+    
+    everything.style.transform = "translate(-50%,-50%) scale("+scale+")";
+    everything.style.display = "block";
+  }
 }
 
 setTimeout(window.onresize, 1);
