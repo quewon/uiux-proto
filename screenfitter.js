55dfee07d4fd959fa033b251a3f7853fafb2e053
quewon <36914642+quewon@users.noreply.github.com>
2022-12-15 00:18:47 +0900

    .

--- screenfitter.js
+++ screenfitter.js
@@ -1,6 +1,11 @@
 window.onresize = function() {
-  var width = window.innerWidth;
-  var height = window.innerHeight;
+  var width = document.documentElement.clientWidth;
+  var height = document.documentElement.clientHeight;
+  
+  // const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  // const height = window.innerHeight || document.documentElement.clientHeight|| document.body.clientHeight;
+  
+  console.log(width, height);
   
   const everything = document.getElementsByClassName("everything")[0];
   const style = window.getComputedStyle(everything);
@@ -9,9 +14,13 @@ window.onresize = function() {
   
   var scaleX = width / sw;
   var scaleY = height / sh;
+  
   var scale = scaleX < scaleY ? scaleX : scaleY;
   
+  console.log(scale)
+  
   everything.style.transform = "translate(-50%,-50%) scale("+scale+")";
+  everything.style.display = "block";
 }
 
-window.onresize();
+setTimeout(window.onresize, 1);
